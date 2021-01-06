caf229cf93198a20e61adb79c99d7ed49ca393e3
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2021-01-06 10:02:15 -0300

    Aula 07 - Emails em desenvolvimento

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,10 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProivder from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from '../providers/MailProvider/models/IMailProvider';
-// import MailProvider from '../providers/MailProvider/implementations/'
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProivder>(
   'DiskStorage',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
