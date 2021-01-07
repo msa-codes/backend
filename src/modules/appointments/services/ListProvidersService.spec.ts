193faa86ef88b2fc0da98393839fa57a8a5c24b0
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2021-01-07 08:59:42 -0300

    Aula 13 - Listagem de prestadores

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUserRepository: FakeUserRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUserRepository = new FakeUserRepository();
+
+    listProviders = new ListProvidersService(fakeUserRepository);
+  });
+
+  it('should be able to list providers', async () => {
+    const user1 = await fakeUserRepository.create({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password',
+    });
+
+    const user2 = await fakeUserRepository.create({
+      name: 'other_name',
+      email: 'other_email@email.com',
+      password: 'other_password',
+    });
+
+    const loggedUser = await fakeUserRepository.create({
+      name: 'loggedUser_name',
+      email: 'loggedUser_email@email.com',
+      password: 'loggedUser_password',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,40 +1,59 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class UsersRepository implements IUsersRepository {
-  private usersRepository: Repository<User>;
+  private ormRepository: Repository<User>;
 
   constructor() {
-    this.usersRepository = getRepository(User);
+    this.ormRepository = getRepository(User);
   }
 
   public async findById(id: string): Promise<User | undefined> {
-    const user = await this.usersRepository.findOne(id);
+    const user = await this.ormRepository.findOne(id);
 
     return user;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = await this.usersRepository.findOne({
+    const user = await this.ormRepository.findOne({
       where: { email },
     });
 
     return user;
   }
 
-  public async create(userData: ICreateUserDTO): Promise<User> {
-    const user = this.usersRepository.create(userData);
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users: User[];
 
-    await this.usersRepository.save(user);
+    if (except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        },
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create(userData);
+
+    await this.ormRepository.save(user);
 
     return user;
   }
 
   public async save(user: User): Promise<User> {
-    return this.usersRepository.save(user);
+    return this.ormRepository.save(user);
   }
 }
 
