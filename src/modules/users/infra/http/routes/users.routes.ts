c09e113813bd81e39c0ccec16f9adc1ccf9a23f6
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2021-01-15 08:11:54 -0300

    Aula 14 - Invalidando cache

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -18,7 +18,7 @@ usersRouter.post(
     [Segments.BODY]: {
       name: Joi.string().required(),
       email: Joi.string().email().required(),
-      password: Joi.string().uuid().required(),
+      password: Joi.string().required(),
     },
   }),
   usersController.create,
--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -16,6 +16,7 @@ class RedisCacheProvider implements ICacheProvider {
 
   public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key);
+    // console.log('recover = ', data);
 
     if (!data) {
       return null;
@@ -27,6 +28,18 @@ class RedisCacheProvider implements ICacheProvider {
   }
 
   public async invalidate(key: string): Promise<void> {}
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
 }
 
 export default RedisCacheProvider;
